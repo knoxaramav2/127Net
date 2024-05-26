b232baab6892763b0024c8e7e487cb7804541e63
KnoxaramaV2 <woodrowtpscott@gmail.com>
2024-05-26 14:55:48 -0500

    Login w/ auth roles

--- 127Net/HomeCore/HomeCore/Data/RoleAuthority.cs
+++ 127Net/HomeCore/HomeCore/Data/RoleAuthority.cs
@@ -8,11 +8,11 @@ namespace HomeCore.Data
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        //public required string RoleName { get; set; }
-        //public int AuthLevel { get; set; }
-        //public bool ForceCredential { get; set; }
-        //public DateTime ReauthTime { get; set; }
-        //public RoleAuthority? Downgrade { get; set; }
+        public required string RoleName { get; set; }
+        public int AuthLevel { get; set; }
+        public bool ForceCredential { get; set; }
+        public DateTime ReauthTime { get; set; }
+        public RoleAuthority? Downgrade { get; set; } = null;
 
         //Referenced by
         public ICollection<UserAccount> OperatingAuthorities { get; } = [];
--- 127Net/HomeCore/HomeCore/Migrations/20240526182728_updateRoleAuth.Designer.cs
+++ 127Net/HomeCore/HomeCore/Migrations/20240526182728_updateRoleAuth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HomeCore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeCore.Migrations
 {
     [DbContext(typeof(HomeCoreDbCtx))]
-    partial class HomeCoreDbCtxModelSnapshot : ModelSnapshot
+    [Migration("20240526182728_updateRoleAuth")]
+    partial class updateRoleAuth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
