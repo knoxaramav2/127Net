52c6f19e515f6b6bfb2a8e5b0bd5d960848f5e70
KnoxaramaV2 <woodrowtpscott@gmail.com>
2024-07-02 23:02:21 -0500

    Attempting conditional testing

--- OTSServer/copyPlugins.ps1
+++ OTSServer/copyPlugins.ps1
@@ -1,15 +1,15 @@
 # Define the list of directories to search for .dll files
 $directories = @(
-    ".\OTSDeviceInfo\bin\release\net8.0",
-    ".\OTSPluginMath\bin\release\net8.0",
-	".\OTSStdProvider\bin\release\net8.0",
-	".\OTSStdLogic\bin\release\net8.0",
-    ".\OTSStdMonitor\bin\release\net8.0"
+    ".\OTSDeviceInfo\bin\debug\net8.0",
+    ".\OTSPluginMath\bin\debug\net8.0",
+	".\OTSStdProvider\bin\debug\net8.0",
+	".\OTSStdLogic\bin\debug\net8.0",
+    ".\OTSStdMonitor\bin\debug\net8.0"
 )
 
 # Define the target directory where .dll files will be copied
-$serverDirectory = "OTSServer\bin\release\net8.0\plugins"
-$testsDirectory = "OTSTests\bin\release\net8.0\plugins"
+$serverDirectory = "OTSServer\bin\debug\net8.0\plugins"
+$testsDirectory = "OTSTests\bin\debug\net8.0\plugins"
 
 # Create the target directory if it doesn't exist
 if (-not (Test-Path -Path $serverDirectory)) {
